fe1e1c1aa1b0fdadcf418e0f9aaa4275f65e399a
‘Lekky <lekkyjay@yahoo.com>
2023-03-26 22:09:11 +0200

    Footer added.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
 import './Footer.scss'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer bg-orange'>
+      <div className = "container py-4 text-center">
+        <div className='flex align-center justify-center text-white fw-3 fs-14'>
+          <Link to = "/" className='text-uppercase'>privacy policy</Link>
+          <div className='vert-line'></div>
+          <Link to = "/" className='text-uppercase'>term of service</Link>
+          <div className='vert-line'></div>
+          <Link to = "/" className='text-uppercase'>About MadinGo.</Link>
+        </div>
+        <span className='text-white copyright-text text-manrope fs-14 fw-3'>&copy; 2022 MadinGo. All Rights Reserved.</span>
+      </div>
+    </footer>
   )
 }
