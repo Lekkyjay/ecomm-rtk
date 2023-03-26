591c1d93e4cd7ca4b2033cb300a2ed30f0163e0a
‘Lekky <lekkyjay@yahoo.com>
2023-03-26 15:35:55 +0200

    Basic layout.

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import Cart from './cart/Cart'
+import CategoryProducts from './categoryProducts/CategoryProducts'
+import Home from './home/Home'
+import ProductDetails from './productDetails/ProductDetails'
+import Search from './search/Search'
+
+export { Cart, CategoryProducts, Home, ProductDetails, Search }
