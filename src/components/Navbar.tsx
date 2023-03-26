22a47e65d1a908a0ce2828ce9fffe931adc4abab
‘Lekky <lekkyjay@yahoo.com>
2023-03-26 18:58:04 +0200

    Added sidebar toggle.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
+import { useAppDispatch } from '../redux/hooks'
+import { setSidebarOn } from '../redux/sidebarSlice'
 import './Navbar.scss'
 
 export default function Navbar() {
+  const dispatch = useAppDispatch()
+
   return (
     <nav className='navbar'>
       <div className='navbar-cnt flex align-center'>
         <div className='brand-and-toggler flex align-center'>
-          <button type = "button" className='sidebar-show-btn text-white'>
+          <button type = "button" className='sidebar-show-btn text-white'  onClick={() => dispatch(setSidebarOn())}>
             <i className='fas fa-bars'></i>
           </button>
           <Link to = "/" className='navbar-brand flex align-center'>
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../../redux/hooks'
+import { selectSidebarStatus, setSidebarOff } from '../../redux/sidebarSlice'
+import './Sidebar.scss'
 
 export default function Sidebar() {
+  const dispatch = useAppDispatch();
+  const isSidebarOn = useAppSelector(selectSidebarStatus);
+
   return (
-    <div>Sidebar</div>
+    <aside className={`sidebar ${isSidebarOn ? 'hide-sidebar' : ""}`}>
+      <button type = "button" className='sidebar-hide-btn' onClick={() => dispatch(setSidebarOff())}>
+        <i className='fas fa-times'></i>
+      </button>
+      <div className='sidebar-cnt'>
+        <div className='cat-title fs-17 text-uppercase fw-6 ls-1h'>All Categories</div>
+        <ul className='cat-list'>
+          <li>
+            <Link to = '' className='cat-list-link text-capitalize'>category here</Link>
+          </li>
+        </ul>
+      </div>
+    </aside>
   )
 }
