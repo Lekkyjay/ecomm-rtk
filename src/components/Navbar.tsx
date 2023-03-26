63ff8ebc459cb94d01cbb6f6f8c733bae27e4d06
‘Lekky <lekkyjay@yahoo.com>
2023-03-26 16:33:56 +0200

    Updated Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,50 @@
+import { Link } from 'react-router-dom'
 import './Navbar.scss'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+      <div className='navbar-cnt flex align-center'>
+        <div className='brand-and-toggler flex align-center'>
+          <button type = "button" className='sidebar-show-btn text-white'>
+            <i className='fas fa-bars'></i>
+          </button>
+          <Link to = "/" className='navbar-brand flex align-center'>
+            <span className='navbar-brand-ico'>
+              <i className='fa-solid fa-bag-shopping'></i>
+            </span>
+            <span className='navbar-brand-txt mx-2'>
+              <span className='fw-7'>Madin</span>Go.
+            </span>
+          </Link>
+        </div>
+
+        <div className='navbar-collapse w-100'>
+          <div className='navbar-search bg-white'>
+            <div className='flex align-center'>
+              <input type = "text" className='form-control fs-14' placeholder='Search your preferred items here' />
+              <Link to = "" className='text-white search-btn flex align-center justify-center'>
+                <i className='fa-solid fa-magnifying-glass'></i>
+              </Link>
+            </div>
+          </div>
+
+          <ul className='navbar-nav flex align-center fs-12 fw-4 font-manrope'>
+            <li className='nav-item no-wrap'>
+              <Link to = '' className='nav-link text-capitalize'>Cat-1</Link>
+              <Link to = '' className='nav-link text-capitalize'>Cat-2</Link>
+              <Link to = '' className='nav-link text-capitalize'>Cat-3</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className='navbar-cart flex align-center'>
+          <Link to = "/cart" className='cart-btn'>
+            <i className='fa-solid fa-cart-shopping'></i>
+            <div className='cart-items-value'>0</div>
+          </Link>
+        </div>
+      </div>
+    </nav>
   )
 }
